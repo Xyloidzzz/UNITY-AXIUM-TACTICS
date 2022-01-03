f885b24c873bacde9e90535545581163f9cc31e4
Alfred <42361881+Xyloidzzz@users.noreply.github.com>
2022-01-03 16:44:47 -0600

    Testing Menu Scripts in Unity Editor

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class MenuScript {
+
+    [MenuItem ("Tools/TestScript")]
+    public static void TestScript () {
+
+        Debug.Log ("Test");
+
+    }
+
+}
